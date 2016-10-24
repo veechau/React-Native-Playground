ea8962707b6dfcf8c7fc7cf6ee09cd6558f6a7f7
Veronica Chau <veronicachau@admins-MacBook-Pro.local>
2016-10-24 14:54:06 -0700

    Add navigation

--- PropertyFinder/index.ios.js
+++ PropertyFinder/index.ios.js
@@ -21,12 +21,28 @@ let styles = ReactNative.StyleSheet.create({
     backgroundColor: 'white',
     fontSize: 30,
     margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
 
+export class HelloWorld extends React.Component {
+  render() {
+    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World (again)!");
+  }
+}
+
 export class PropertyFinderApp extends React.Component {
   render() {
-    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World!");
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: HelloWorld,
+        }}/>
+    );
   }
 }
 
